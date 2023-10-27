28f79f3c453527437456048332ac18dcc91ee8f5
DouglasFerreir <147665862+DouglasFerreir@users.noreply.github.com>
2023-10-27 16:36:10 +0000

    rascunho

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+let pessoa = {
+    nome: "Douglas",
+    altura: 3,
+    nacionalidade: "cubano",
+    idade: 50,
+    envelhecer() {
+        this.idade += 1;
+        console.log(this.idade);
+    }
+};
+pessoa.envelhecer;
+pesquisar;
+constructor, private, javascript,
+    class Openbank {
+    };
