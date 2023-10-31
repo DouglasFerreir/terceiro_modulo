7f1f95cc91fc737a61ed7b1df9d210fc2738f2ad
DouglasFerreir <147665862+DouglasFerreir@users.noreply.github.com>
2023-10-31 00:32:40 +0000

    30.10.23

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
-export class transaction {
-private _idRemetente: string
-private _IdDestinatario:string
-private _Valor: number
-private _dataDeCriacao: new Date()
+import{Bankaccount} from "./classes/BankAccount";
+import{Transaction} from "./classes/transactions";
 
-constructor(
-    idRemetente:string,
-    IdDestinatario:string,
-    Valor:number,
+const ba1 = new BankAccount("Douglas", "dgfhfhdhdfjjd", "12233456")
+const ba2 = new BankAccount("Douglas", "dgfhfhddjsdkmskd", "19852621546)
+const b3 = new transaction("d454ghhjdmasj", "sgjd845764", 200)
+)
 
-) {
-this._idRemetente = idRemetente
-this._IdDestinatario = IdDestinatario
-this._Valor = 0
-}
-}
+function transaction(
+    idDestinatário:string
+    idRemetente:string
+    valor: number 
+)
+
+return this.valor
+
+
+ function BankAccount
+ nome: string
+      : dgfhfhdhdfjjd
+      : 12233456
+ 
+
+
+Construction 
+this.idDestinatário=3657
+this.idRemetente=6967
+this.valor=0
+
+
+class newAccount
+nome: string 
+new Date: 
