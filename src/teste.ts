7e4de41b9c3bf07a32c351b4a70429f34adee035
DouglasFerreir <147665862+DouglasFerreir@users.noreply.github.com>
2023-11-08 00:25:54 +0000

    teste

---
+++ src/teste.ts
@@ -0,0 +1,19 @@
+// smartphone = objeto
+// 3 propriedades desse objeto
+
+const smartphone {
+    marca = Apple
+    modelo: Iphone15
+    internet5G: Sim
+}
+    // metodo com parametro
+
+    mensagem (texto: string) {
+     console.log mensagem("O(a) senhor(a) recebeu a seguinte mensagem:", mensagem)
+}
+   
+//  metodo sem parametro
+
+discagem() {
+     console.log discagem("discando:", discagem)
+}
